caf642b83950244630310c0022457f60afbb24fd
Gabriela Ordonez <00361516@uca.edu.sv>
2019-03-16 09:40:53 -0600

    Initial commit

---
+++ app/src/main/java/com/gordonez/paso_bebe_grupal/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gordonez.paso_bebe_grupal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
